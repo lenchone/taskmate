1c1cded2add7276a18353e2430aa1905ad850a49
Lencho <lenchonatei@gmail.com>
2022-09-09 16:12:25 +0300

    Project updated

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import django_heroku
 import os
 from pathlib import Path
 import environ
@@ -136,3 +137,5 @@ CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_REDIRECT_URL = "todolist"
 
 LOGIN_URL='login'
+
+django_heroku.settings(locals()) 
